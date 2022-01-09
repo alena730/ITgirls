bdbf8b62e7d945178d557139ab9d6897ce5677de
alena730 <91200779+alena730@users.noreply.github.com>
2022-01-09 12:05:36 +0100

    Practice week 13

---
+++ week13/calculator/cal.js
@@ -0,0 +1,24 @@
+let number1 = Number(str)
+let number2 = Number(str)
+
+let x=0;
+
+function plus()
+{
+    let x=number1+number2;
+}
+
+function minus()
+{
+    let x=number1-number2;
+}
+
+function times()
+{
+    let x=number1*number2;
+}
+
+function divide()
+{
+    let x=number1/number2;
+}
